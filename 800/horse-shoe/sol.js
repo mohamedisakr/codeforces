3f53e60f1cc9418cf51a35ce45d26fc4b14361ab
mohamedisakr <mohamedisakr@gmail.com>
2022-04-18 09:41:39 +0200

    solving horse shoe

---
+++ 800/horse-shoe/sol.js
@@ -0,0 +1,32 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function main() {
+  let arr = readLine().split(" ").map(Number);
+  let set = new Set(arr);
+  console.log(arr.length - set.size);
+}
